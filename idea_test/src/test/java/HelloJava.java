845ce5d483e855228383929461477b3c796ae0e1
hehuiling1004 <2636307532@qq.com>
2021-10-26 19:50:42 +0800

    第一次提交

---
+++ idea_test/src/test/java/HelloJava.java
@@ -0,0 +1,17 @@
+import sun.applet.Main;
+import sun.misc.PostVMInitHook;
+
+import javax.sound.midi.SysexMessage;
+
+/**
+ * @program: IDEA_code
+ * @description:
+ * @author: hhl
+ * @create: 2021-10-25 17:14
+ **/
+public class HelloJava {
+    public static void main(String srg){
+        System.out.println("hello hhh");
+    }
+
+}
